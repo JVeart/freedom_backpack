b6d922c10f06b44ac951f23a1c97521cfc6c49e6
dredupuika <andrievs.niedra@inbox.lv>
2014-03-26 21:21:18 +0200

    just some basic stuff

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, target-densitydpi=device-dpi, initial-scale=1" name="viewport">
+            
+        <? // just a sample of fb meta tags ?>
+        <meta content="Jveart" property="og:title">
+        <meta content="website" property="og:type">
+        <meta content="http://www.jveart.com" property="og:url">
+        <meta content="http://www.jveart.com/images/jveart_fb.jpg" property="og:image">
+        <meta content="Jveart" property="og:site_name">
+        <meta content="100000401876002" property="fb:admins">
+        <meta content="We're Here For Your Music To Be Heard" property="og:description">
+    
+        <title>title</title>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css" />
+        <link type="text/css" rel="stylesheet" href="/css/style.css?<?php echo ver; ?>" />
+        
+        <script content="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script content="text/javascript" src="/libs/jquery-2.1.0.js"><\/script>')</script>
+        <!--[if lt IE 9]>
+            <script type="text/javascript" src="/libs/html5shiv.js"></script>
+        <![endif]-->
+        
+        <script type="text/javascript" src="/js/script.js?<?php echo ver; ?>"></script>
+        
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define("ver",     "0.1");
+
+include('header.php');
+include('content.php');
+include('footer.php');
+
+?>
